6bd6bb22da2a313089a6cf424023af3675abe8a3
victory-vi <70841829+victory-vi@users.noreply.github.com>
2020-10-03 17:56:15 +0300

    init: make function body

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
-  return 0;
+if (Array.isArray(array)) {
+        if (array.length == 0) {
+            return 0;
+        } else
+        if (array.length > 0) {
+            minElement = array[0];
+            for (var i = 0; i < array.length; i++) {
+                if (minElement > array[i]) {
+                    minElement = array[i];
+                }
+            }
+        }
+        return minElement;
+    } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+ if (Array.isArray(array)) {
+        if (array.length == 0) {
+            return 0;
+        } else
+        if (array.length > 0) {
+            maxElement = array[0];
+            for (var i = 0; i < array.length; i++) {
+                if (maxElement < array[i]) {
+                    maxElement = array[i];
+                }
+            }
+        }
+        return maxElement;
+    } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (Array.isArray(array)) {
+        if (array.length == 0) {
+            return 0;
+        } else if (array.length > 0) {
+            element = 0;
+            for (var i = 0; i < array.length; i++) {
+                element += array[i];
+            }
+            return element / (array.length);
+        }
+
+    } else return 0;
 }
